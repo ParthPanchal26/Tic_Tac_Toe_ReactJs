30298d2ee1c96d0dda045ed22ffbad47decbf6a1
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-12-24 23:48:50 +0530

    Completed Tic Tac Toe Game project with better UI

--- src/Tic_Tac_Toe/Board.jsx
+++ src/Tic_Tac_Toe/Board.jsx
@@ -6,31 +6,78 @@ const Board = () => {
     const [state, setState] = React.useState(Array(9).fill(null));
     const [isXTurn, setIsXTurn] = React.useState(true);
 
+    const checkWinner = () => {
+        const winnerLogic = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for (let logic of winnerLogic) {
+            const [a, b, c] = logic
+            if (state[a] !== null && state[a] === state[b] && state[a] === state[c])
+                return state[a]
+        }
+
+        return false
+    }
+
+    const isWinner = checkWinner()
+
     const handleClick = (index) => {
+
+        if (state[index] !== null) return
+
         const copyState = [...state]
         copyState[index] = isXTurn ? 'X' : 'O'
         setState(copyState)
         setIsXTurn(!isXTurn)
     }
 
+    const playAgain = () => {
+        setState(Array(9).fill(null))
+    }
+
 
     return (
+
         <div className="board-container">
-            <div className="board-row">
-                <Square onClick={() => handleClick(0)} value={state[0]} />
-                <Square onClick={() => handleClick(1)} value={state[1]} />
-                <Square onClick={() => handleClick(2)} value={state[2]} />
-            </div>
-            <div className="board-row">
-                <Square onClick={() => handleClick(3)} value={state[3]} />
-                <Square onClick={() => handleClick(4)} value={state[4]} />
-                <Square onClick={() => handleClick(5)} value={state[5]} />
-            </div>
-            <div className="board-row">
-                <Square onClick={() => handleClick(6)} value={state[6]} />
-                <Square onClick={() => handleClick(7)} value={state[7]} />
-                <Square onClick={() => handleClick(8)} value={state[8]} />
-            </div>
+            <div className='heading'><h2>Tic Tac Toe</h2></div>
+
+            {isWinner ?
+                <>
+                    <div className="board-row-tracker border-bottom">
+                        <h3>{isWinner} Won</h3>
+                        <button onClick={playAgain}>Play again</button>
+                    </div>
+                </> :
+                <>
+                    <div className='board-row-tracker'>
+                        <h3>Player {isXTurn ? "X" : "O"} Turn</h3>
+                        <button onClick={playAgain}>ReStart Game</button>
+                    </div>
+                    <div className="board-row">
+                        <Square onClick={() => handleClick(0)} value={state[0]} style={{ borderTopLeftRadius: '0.3rem' }} />
+                        <Square onClick={() => handleClick(1)} value={state[1]} />
+                        <Square onClick={() => handleClick(2)} value={state[2]} style={{ borderTopRightRadius: '0.3rem' }} />
+                    </div>
+                    <div className="board-row">
+                        <Square onClick={() => handleClick(3)} value={state[3]} />
+                        <Square onClick={() => handleClick(4)} value={state[4]} />
+                        <Square onClick={() => handleClick(5)} value={state[5]} />
+                    </div>
+                    <div className="board-row">
+                        <Square onClick={() => handleClick(6)} value={state[6]} style={{ borderBottomLeftRadius: '0.3rem' }} />
+                        <Square onClick={() => handleClick(7)} value={state[7]} />
+                        <Square onClick={() => handleClick(8)} value={state[8]} style={{ borderBottomRightRadius: '0.3rem' }} />
+                    </div>
+                </>
+            }
         </div>
     )
 }
--- src/Tic_Tac_Toe/Square.jsx
+++ src/Tic_Tac_Toe/Square.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
 const Square = (props) => {
   return (
-    <div onClick={props.onClick} className="square">
+    <div style={props.style} onClick={props.onClick} className="square">
         <h5>{props.value}</h5>
     </div>
   )
